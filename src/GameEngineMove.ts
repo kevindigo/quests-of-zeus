182c83d025b369487f1c3551966a249c88f91910
kevindigo <kevins@qualitycode.com>
2025-11-27 11:27:29 +0100

    Fix where move highlighting wasn't tied to selected resource

--- src/GameEngineMove.ts
+++ src/GameEngineMove.ts
@@ -1,51 +1,43 @@
 import type { Action, ShipMoveAction } from './actions.ts';
 import type { GameState } from './GameState.ts';
 import { MovementSystem } from './MovementSystem.ts';
-import { Resource } from './Resource.ts';
 import { Failure, type ResultWithMessage } from './ResultWithMessage.ts';
 import { ShipMoveHandler } from './ShipMoveHandler.ts';
 import type { PossibleShipMove } from './types.ts';
 
 export class GameEngineMove {
   public static getMoveActions(gameState: GameState): Action[] {
+    const possibleActionsWithAvailableResources: ShipMoveAction[] = [];
+
     const player = gameState.getCurrentPlayer();
     const availableFavor = player.favor;
     const availableResources = player.getAvailableResourcesWithRecoloring();
-    console.log(`GEM.gME: Evalating ${availableResources.length} resources`);
-    const allPossibleMoves = this.getAllPossibleShipMovesWithFavor(gameState);
-    const availableMoves = allPossibleMoves.filter((move) => {
+
+    const allTheoreticallyPossibleMoves = this.getAllPossibleShipMovesWithFavor(
+      gameState,
+    );
+
+    allTheoreticallyPossibleMoves.forEach((move) => {
       const suitableResource = availableResources.find((resource) => {
-        console.log(
-          `GEM.gME: Move 
-          ${JSON.stringify(move.effectiveColor)} (${move.favorCost}) vs. 
-          resource ${resource.getBaseColor()} (${resource.getRecolorCost()})`,
-        );
-        if (
+        return (
           move.effectiveColor === resource.getEffectiveColor() &&
           move.favorCost + resource.getRecolorCost() <= availableFavor
-        ) {
-          console.log('GEM.gME: Yes!');
-          return resource;
-        }
-        console.log('GEM.gME: Nope!');
-        return null;
+        );
       });
-      return suitableResource ? true : false;
-    });
-    const availableMoveActions: ShipMoveAction[] = availableMoves.map(
-      (move) => {
+
+      if (suitableResource) {
         const action: ShipMoveAction = {
           type: 'move',
           subType: 'shipMove',
           destination: { q: move.q, r: move.r },
-          spend: Resource.none,
+          spend: suitableResource,
           favorToExtendRange: move.favorCost,
         };
-        return action;
-      },
-    );
-    // FixMe: Need to filter out any moves where recoloring+rangeExtension > available favor
-    return availableMoveActions;
+        possibleActionsWithAvailableResources.push(action);
+      }
+    });
+
+    return possibleActionsWithAvailableResources;
   }
 
   public static doAction(
