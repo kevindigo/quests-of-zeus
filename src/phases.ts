b50706389aef6d83a06b9279b76f70f3687447f3
kevindigo <kevins@qualitycode.com>
2025-12-02 09:11:59 +0100

    Draft WIP phases

---
+++ src/phases.ts
@@ -0,0 +1,74 @@
+import type { Action } from './actions.ts';
+import { GameEngineAnyResource } from './GameEngineAnyResource.ts';
+import { GameEngineColor } from './GameEngineColor.ts';
+import { GameEngineFree } from './GameEngineFree.ts';
+import { GameEngineHex } from './GameEngineHex.ts';
+import { GameEngineMove } from './GameEngineMove.ts';
+import type { GameState } from './GameState.ts';
+
+// each player will have:
+//  export const phaseQueue: Phase[] = [];
+
+export interface Phase {
+  getName(): string;
+  getAvailableActions(GameState: GameState): Action[];
+}
+
+export function createPhase(phaseName: string): Phase {
+  if (phaseName === 'main') {
+    return new PhaseMain();
+  }
+
+  throw new Error('Cannot create unknown phase: ' + phaseName);
+}
+
+export class PhaseMain implements Phase {
+  public getName(): string {
+    return 'main';
+  }
+
+  public getAvailableActions(GameState: GameState): Action[] {
+    const actions: Action[] = [];
+    actions.push(...GameEngineFree.getFreeActions(GameState));
+    actions.push(...GameEngineAnyResource.getAnyResourceActions(GameState));
+    actions.push(...GameEngineHex.getHexActions(GameState));
+    actions.push(...GameEngineMove.getMoveActions(GameState));
+    actions.push(...GameEngineColor.getColorActions(GameState));
+    return actions;
+  }
+}
+
+// export class PhaseAdvancingGod implements Phase {
+//   private allowedColors: CoreColor[];
+//   private canAdvanceFromZero: boolean;
+//   private remainingCount: number;
+// }
+
+// export class PhaseTeleporting implements Phase {
+// }
+
+// export class PhasePeeking implements Phase {
+// }
+
+// export class PhaseExploringAnyShrine implements Phase {
+// }
+
+// export class PhaseGrabbingStatue implements Phase {
+//   private allowedColors: CoreColor[];
+// }
+
+// export class PhaseGrabbingOffering implements Phase {
+//   private allowedColors: CoreColor[];
+// }
+
+// export class PhaseGainingEquipment implements Phase {
+// }
+
+// export class PhaseGainingCompanion implements Phase {
+// }
+
+// export class PhaseHealingColor implements Phase {
+// }
+
+// export class PhaseFightingMonster implements Phase {
+// }
